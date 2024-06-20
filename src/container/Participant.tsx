2a5d226167d6824a3f7ec3618a99dcfbae2327b6
DragonSlayer <varattleff@Viktors-MacBook-Air.local>
2024-06-20 19:59:23 +0200

    search participants

--- src/container/Participant.tsx
+++ src/container/Participant.tsx
@@ -1,10 +1,17 @@
 import { DataGrid } from '@mui/x-data-grid';
 import useParticipant from "../hooks/useParticipant.tsx";
 import LoadingSpinner from "../components/LoadingSpinner.tsx";
-import { Paper } from "@mui/material";
+import { Paper, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 function Participant() {
     const { participants, isLoading } = useParticipant();
+    const [searchText, setSearchText] = useState('');
+
+    const handleSearchChange = (e:  React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setSearchText(e.target.value);
+    };
+
 
     const columns = [
         { field: 'fullName', headerName: 'Full Name', width: 200 },
@@ -19,7 +26,17 @@ function Participant() {
 
     ];
 
-    const rows = participants.map(participant => ({
+    const filteredParticipants = participants.filter(participant =>
+        participant.fullName.toLowerCase().includes(searchText.toLowerCase()) ||
+        participant.age.toString().includes(searchText) ||
+        participant.gender.toLowerCase().includes(searchText.toLowerCase()) ||
+        participant.adjacentClub.toLowerCase().includes(searchText.toLowerCase()) ||
+        participant.ageGroup.toLowerCase().includes(searchText.toLowerCase()) ||
+        participant.country.toLowerCase().includes(searchText.toLowerCase()) ||
+        participant.disciplines.some(discipline => discipline.name.toLowerCase().includes(searchText.toLowerCase()))
+    );
+
+    const rows = filteredParticipants.map(participant => ({
         id: participant.id,
         fullName: participant.fullName,
         age: participant.age,
@@ -41,7 +58,32 @@ function Participant() {
                         borderRadius: 2
                     }}
                 >
-                    <DataGrid rows={rows} columns={columns}   />
+                    <TextField
+                        label="Search"
+                        variant="outlined"
+                        value={searchText}
+                        onChange={(e) => handleSearchChange(e)}
+
+                    />
+
+
+                    <Paper
+                        elevation={3}
+                        sx={{
+                            padding: 2,
+                            margin: 2,
+                            borderRadius: 2
+                        }}
+                    >
+
+
+
+                            <DataGrid rows={rows} columns={columns} autoHeight />
+
+
+                    </Paper>
+
+
                 </Paper>
 
         </>
